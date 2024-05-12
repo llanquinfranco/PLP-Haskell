0250993933b0e632cb91be2187c3ed2a322dc405
llanquinfranco <llanquinf@gmail.com>
2024-05-12 01:00:52 -0300

    nombre modulo

--- ejercicio24.hs
+++ ejercicio24.hs
@@ -1,21 +1,21 @@
 {- Definir una funcion tal que, dado un numero entero, genere la siguiente salida 
 repitiendo la cantidad de dıgitos con una lınea para cada dıgito -}
-linea :: Int -> Int -> String
-linea n m
-    | (m==0) = ""
-    | (m==1) = show n
-    | otherwise = show n ++ linea n (m-1)
-
-ej24 :: Int -> String
-ej24 n
+cantDigitos :: Int -> String
+cantDigitos n
     | (n < 10) = linea n n 
-    | (n > 10) = ej24 (n `div` 10) ++ "\n" ++ linea m m
+    | (n > 10) = cantDigitos (n `div` 10) ++ "\n" ++ linea m m
     | otherwise = ""
   where 
     m = (n `mod` 10)
+
+linea :: Int -> Int -> String
+linea n m
+    | (m == 0) = ""
+    | (m == 1) = show n
+    | otherwise = show n ++ linea n (m - 1)
 
 main :: IO ()
 main = do
     putStrLn "Ingrese el digito"
     m <- readLn
-    putStrLn (ej24 m)
+    putStrLn (cantDigitos m)
