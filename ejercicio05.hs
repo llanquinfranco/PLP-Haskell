ca9adbd4384c166763298a188aea6523737edc54
llanquinfranco <llanquinf@gmail.com>
2024-06-15 17:39:10 -0300

    pre recu

--- ejercicio05.hs
+++ ejercicio05.hs
@@ -10,11 +10,10 @@ factorial n
     | (n == 1) = 1
     | (n > 1) = n * factorial(n - 1)
 
-sucesion :: Int -> Int -> Int
-sucesion x n = (sumatoria n n) / (factorial x)
+sucesion :: Int -> Int -> Float
+sucesion x n = fromIntegral(sumatoria n n) / fromIntegral(factorial x)
 
 sumatoria :: Int -> Int -> Int
 sumatoria i n
-
-
-
+    | (i >= 1) = (i ^ n) + sumatoria (i - 1) n
+    | otherwise = 0
